b004aad8c29f280d14537d7d334266eb75564701
Swapnil Kshirsagar <kshirsagarswapnil@email.com>
2025-06-19 21:44:04 +0530

    Fix: set runtime to node for inngest route

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/Config/inngest";
 
+export const runtime = "node";
+
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
