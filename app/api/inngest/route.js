ef88cfdb7d655b489f70e4f0b3fc08a2f3cad3c0
Swapnil Kshirsagar <kshirsagarswapnil@email.com>
2025-06-20 20:34:34 +0530

    first commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,7 +2,7 @@ export const runtime = "nodejs";
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/Config/inngest";
 
-// Serve Inngest functions
+// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
